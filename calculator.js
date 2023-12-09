714fb0291a755b9fa738261ac841fa75d290547e
Wajih-Khalil <134858920+Wajih-Khalil@users.noreply.github.com>
2023-12-10 02:38:30 +0500

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,18 @@
+//Calculator clear, add and error option
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
